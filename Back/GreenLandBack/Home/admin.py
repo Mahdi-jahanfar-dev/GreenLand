03c6149ca51a5f782d4958f1ac61612ee130d2af
Mahdi-jahanfar-dev <mahdi.jahanfar01@gmail.com>
2025-06-03 20:52:02 +0330

    feat: added update zone model

--- Back/GreenLandBack/Home/admin.py
+++ Back/GreenLandBack/Home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Zone, GreenLand, SetRole
+from .models import Zone, GreenLand, SetRole, ZoneUpdate
 
 admin.site.register(Zone)
 admin.site.register(GreenLand)
 admin.site.register(SetRole)
+admin.site.register(ZoneUpdate)
