57ccdac1b07b1d3e2342c35f4f9a115c83fd29af
Mahdi-jahanfar-dev <mahdi.jahanfar01@gmail.com>
2025-08-10 17:49:23 +0330

    feat: added user register view

--- Back/GreenLandBack/Account/serializers.py
+++ Back/GreenLandBack/Account/serializers.py
@@ -11,3 +11,28 @@ class ProfileSerilizer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['username', 'full_name', 'email', 'is_staff', 'is_superuser', 'allow_users_add_greenlands', 'green_land', 'role']
         read_only_fields = ['is_staff', 'is_superuser']
+        
+        
+        
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length = 20, required = True, write_only = True, min_length = 8)
+    password_2 = serializers.CharField(max_length = 20, required = True, write_only = True, min_length = 8)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'full_name', 'email', 'password', 'password_2']
+        
+        def validate(self, data):
+            if data['password'] != data['password_2']:
+                raise serializers.ValidationError({"password_confirmation": "Passwords do not match."})
+            return data
+        
+        def create(self, validated_data):
+            validated_data.pop('password_2')
+            user = self.model.create_user(
+                username = validated_data['username'],
+                full_name = validated_data['full_name'],
+                email = validated_data['email'],
+                password = validated_data['password']
+            )
+            return user
--- Back/GreenLandBack/Account/views.py
+++ Back/GreenLandBack/Account/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import ProfileSerilizer
+from .serializers import ProfileSerilizer, UserSerializer
 from .models import CustomUser
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+
+
+class UserRegisterView(APIView):
+    
+    model = CustomUser
+    serializer_class = UserSerializer
+    
+    def post(self, request):
+        
+        serializer = self.serializer_class(data = request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'user created'}, status=status.HTTP_201_CREATED)
+        return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -15,7 +29,7 @@ class UserProfile(APIView):
     def get(self, request, id):
         user = get_object_or_404(self.model, id = id)
         if request.user.id == user.id:
-            serializer = self.serializer_class(self.model.objects.get(id = id))
+            serializer = self.serializer_class(user)
             return Response(serializer.data)
         else:
             return Response({'error': 'not found'}, status=status.HTTP_404_NOT_FOUND)
